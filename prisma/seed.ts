85caaf30b87d7294feea336427bb609fb614f4d5
Airuxebu <michaelvh2026@gmail.com>
2026-05-12 16:06:22 +0200

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,7 @@ import { PrismaClient, UserRole } from 'src/generated/prisma/client';
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg({ connectionString }, { schema: 'claude' });
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
